b1312219f390c557aa01bde6d448e04707f29263
Tam <studio@tamlove.com>
2021-06-22 16:28:41 +0100

    Added Redux library, store and Provider.

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Tabs } from './routes/router'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers/decks'
 
 export default function App() {
+  const store = createStore(reducer)
   return (
-    <NavigationContainer>
-       <View style={styles.container}>
-        <StatusBar style="auto" />
+    <Provider store={store}>
+      <View>
+          <StatusBar backgroundColor='#000000' />
       </View>
+      <NavigationContainer>
       <Tabs />
     </NavigationContainer>
-  );
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
